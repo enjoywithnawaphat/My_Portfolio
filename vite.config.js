cd468cefe089bcb518f8bfac91340a540ae4d82c
enjoywithnawaphat <nigth7821@gmail.com>
2026-09-07 21:52:22 +0700

    Delete vite.config

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  // เปลี่ยน /nawaphat-portfolio/ เป็นชื่อ Repository ใน GitHub ของคุณ
-  base: '/My_Portfolio/', 
-})
